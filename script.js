ee0b8d251a5105b734b8bffd5b5f6641c7985f3e
Anthony Jiang <tmxkn1@users.noreply.github.com>
2022-04-27 22:43:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Researcher-app open article in new tab
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Add open article in new tab link to the paper
-// @author       Anthony Jiang
-// @match        https://www.researcher-app.com/feed/journal*
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.researcher-app.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=researcher-app.com
 // @grant        none
 // ==/UserScript==
@@ -18,17 +18,16 @@ function addPaperLink(node) {
     'use strict';
     const docMutationCallback = function(mutationsList, observer) {
         for (const mutation of mutationsList) {
-            if (mutation.type !== 'childList') {
-                continue;
-            }
-            for (const node of mutation.addedNodes) {
-                try {
-                    if (node.getAttribute('x-track-id') === 'paper') {
-                        addPaperLink(node);
-                    }
-                } catch (err) {
-                    if (err.name == 'TypeError') {
-                        continue;
+            if (mutation.type === 'childList') {
+                for (const node of mutation.addedNodes) {
+                    try {
+                        if (node.getAttribute('x-track-id') === 'paper' && document.location.href.endsWith('feed/all')) {
+                            addPaperLink(node);
+                        }
+                    } catch (err) {
+                        if (err.name == 'TypeError') {
+                            continue;
+                        }
                     }
                 }
             }

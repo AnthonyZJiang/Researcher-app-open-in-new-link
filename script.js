eea93ead1a8401b4526002af38555cbb7947ef4a
Anthony Jiang <tmxkn1@users.noreply.github.com>
2022-04-28 02:15:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Researcher-app open article in new tab
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.researcher-app.com/*
